e2dfd7cfd00a5b0bf1b9e7c34cb7f0bc0fa7ac2e
Wally Tavares <dev.wallyst@gmail.com>
2025-12-08 15:02:18 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,30 @@
 import { Router } from "express";
 import * as Station from "./controllers/stationController.js";
 import * as Auth from "./controllers/authController.js";
+import * as Device from "./controllers/deviceController.js";
 
 const routes = Router();
 
-// Rota para teste rápido se a API está online
+// ------------------ Rota básica ------------------
 routes.get("/", (req, res) => {
   res.json({ status: "BeerControl API online 🍺" });
 });
 
-// Login simples (futuramente vamos ligar com banco de dados)
+// ------------------ Auth -------------------------
 routes.post("/login", Auth.login);
 
-// Geração de token para estação
+// ------------------ Station / Tokens -------------
 routes.post("/station/token", Station.generateToken);
+
+// ------------------ Ping p/ ESP32 ----------------
+// Usado pelo firmware no comando SERVER_TEST (GET em /api/ping)
+routes.get("/api/ping", Device.ping);
+
+// ------------------ Dispositivos / Serial --------
+// Ativação de dispositivo (gera ou reutiliza serial)
+routes.post("/device/activate", Device.activateDevice);
+
+// Consulta por MAC (debug / suporte)
+routes.get("/device/by-mac/:mac", Device.getDeviceByMac);
 
 export default routes;
